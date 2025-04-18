e1960f7d6e13dc0a2b1387397289e9d032c6f5ec
yaswanthrajeev <117917606+yaswanthrajeev@users.noreply.github.com>
2025-04-18 18:15:28 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,9 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `Hey I'm Yaswanth Rajeev, a freshman at Model Engineering College, Thrikkakara.I'm a passionate computer science student with strong problem-solving skills and a desire to continuously learn and grow in the field. Seeking internships and entry-level opportunities to apply my knowledge and contribute to innovative projectsI am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Hey I'm Yaswanth Rajeev, a freshman at Model Engineering College, Thrikkakara.I'm a passionate computer science student with strong problem-solving skills and a desire to continuously learn and grow in the field. Seeking internships and entry-level opportunities to apply my knowledge and contribute to innovative projectsI am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Flask, MySQL, . My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications.I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications.I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, Python, Opencv. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 /*
 export const EXPERIENCES = [
   {
@@ -55,7 +55,7 @@ export const PROJECTS = [
   },
   {
     title: "Subtitle Generator",
-    image: project5,
+    image: project1,
     description:
       "Automatic subtitle generator that converts speech to synced text using speech recognition.",
     technologies: ["Whisper", "Python","Flask","numpy"],
