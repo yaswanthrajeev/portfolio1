3e071940680986710a822bd05a2a621d7bc002af
yaswanthrajeev <117917606+yaswanthrajeev@users.noreply.github.com>
2025-04-18 18:10:13 +0530

    Update projects and experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,7 @@ import project4 from "../assets/projects/project-4.jpg";
 export const HERO_CONTENT = `Hey I'm Yaswanth Rajeev, a freshman at Model Engineering College, Thrikkakara.I'm a passionate computer science student with strong problem-solving skills and a desire to continuously learn and grow in the field. Seeking internships and entry-level opportunities to apply my knowledge and contribute to innovative projectsI am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications.I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
-
+/*
 export const EXPERIENCES = [
   {
     year: "2023 - Present",
@@ -37,7 +37,7 @@ export const EXPERIENCES = [
     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
   },
 ];
-
+*/
 export const PROJECTS = [
   {
     title: "Blogging Platform",
@@ -45,6 +45,20 @@ export const PROJECTS = [
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles. ",
     technologies: ["HTML", "CSS", "React", "firebase"],
+  },
+  {
+    title: "Pedestrian Detection",
+    image: project2,
+    description:
+      "Real-time pedestrian detection system using computer vision to enhance safety and automation.",
+    technologies: ["Next.js", "Python", "Opencv", "Flask","Supabase"],
+  },
+  {
+    title: "Subtitle Generator",
+    image: project5,
+    description:
+      "Automatic subtitle generator that converts speech to synced text using speech recognition.",
+    technologies: ["Whisper", "Python","Flask","numpy"],
   },
   {
     title: "TIK TAK TOE GAME",
@@ -60,13 +74,7 @@ export const PROJECTS = [
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML", "CSS", "React", "Tailwind"],
   },
-  {
-    title: "LIBRARY SYSTEM",
-    image: project2,
-    description:
-      "A simple library management system where users can add ,delete or update books.",
-    technologies: ["HTML",'CSS','REACT','NODE'],
-  },
+ 
 ];
 
 export const CONTACT = {
