2aa626f7e42426bcfb8acaf7d435ee4d13475ad6
yaswanthrajeev <117917606+yaswanthrajeev@users.noreply.github.com>
2025-06-28 12:00:44 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,62 +3,36 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `Hey I'm Yaswanth Rajeev, a freshman at Model Engineering College, Thrikkakara.I'm a passionate computer science student with strong problem-solving skills and a desire to continuously learn and grow in the field. Seeking internships and entry-level opportunities to apply my knowledge and contribute to innovative projectsI am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Flask, MySQL, . My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Hey, I'm Yaswanth Rajeev, a freshman at Model Engineering College, Thrikkakara. I'm a passionate computer science student with strong problem-solving skills and a desire to continuously learn and grow in the field. Seeking internships and entry-level opportunities to apply my knowledge and contribute to innovative projectsI am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Flask, MySQL, . My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications.I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, Python, Opencv. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
-/*
+export const ABOUT_TEXT = `I am a dedicated and versatile full-stack developer with a passion for creating efficient and user-friendly web applications.I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, Python, Opencv. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "2025 June 16 - June 27",
+    role: "Full Stack Developer Intern",
+    company: "Gadgeon Smart Systems Pvt. Ltd.",
+    description: `Worked on building a full-stack microservices-based web application with real-time sentiment analysis. Developed the frontend with React.js and
+    Tailwind CSS, implemented the backend using FastAPI and SQLite, integrated RabbitMQ for messaging, and deployed the solution using Docker
+    and GitHub Pages.`,
+    technologies: ["FastAPI", "React.js", "RabbitMQ", "Docker", "Hugging Face"],
   },
 ];
-*/
+
 export const PROJECTS = [
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles. ",
-    technologies: ["HTML", "CSS", "React", "firebase"],
-  },
   {
     title: "Pedestrian Detection",
     image: project2,
     description:
       "Real-time pedestrian detection system using computer vision to enhance safety and automation.",
-    technologies: ["Next.js", "Python", "Opencv", "Flask","Supabase"],
+    technologies: ["Next.js", "Python", "Opencv", "Flask", "Supabase"],
   },
   {
     title: "Subtitle Generator",
     image: project1,
     description:
       "Automatic subtitle generator that converts speech to synced text using speech recognition.",
-    technologies: ["Whisper", "Python","Flask","numpy"],
+    technologies: ["Whisper", "Python","Flask", "numpy"],
   },
   {
     title: "TIK TAK TOE GAME",
@@ -68,11 +42,11 @@ export const PROJECTS = [
     technologies: ["HTML","CSS","REACT"],
   },
   {
-    title: "Portfolio Website",
+    title: "MicroForum Platform",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Tailwind"],
+      "A Microforum platform with sentiment analysis",
+     technologies: ["FastAPI", "React.js", "RabbitMQ", "Docker", "Hugging Face"],
   },
  
 ];
